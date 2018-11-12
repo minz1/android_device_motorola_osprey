22147ea98e267cdd21bd9665d266154e6e2e7a58
Kyle Harrison <khwebmail@gmail.com>
2018-11-12 16:56:10 +0000

    osprey: Consider gzosp_osprey product via the AOSP way

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
-# Copyright (C) 2015-2016 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+#
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,17 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/motorola/osprey/full_osprey.mk)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/gzosp_osprey.mk
 
-# Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
-PRODUCT_MODEL := MotoG3
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := osprey
