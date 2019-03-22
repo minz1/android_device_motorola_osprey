9b3886601f8a55d286ae02aca4ba269859b4720a
Kyle Harrison <khwebmail@gmail.com>
2019-03-22 14:58:19 +0000

    DU => REVENGEOS

--- revengeos_osprey.mk
+++ revengeos_osprey.mk
@@ -15,17 +15,15 @@
 
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
 
-# Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
-
-# Use specific resolution for bootanimation
-SMALL_BOOTANIMATION_SIZE := 720x720
+# Inherit some common RevengeOS stuff.
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm
+TARGET_MINIMAL_APPS := true
+TARGET_BOOT_ANIMATION_RES := 720
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := du_osprey
+PRODUCT_NAME := revengeos_osprey
 PRODUCT_MODEL := MotoG3
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
